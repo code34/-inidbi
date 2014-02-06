4877824cb2ebfab05f9a892cbc98ee0377740dd3
Nicolas BOITEUX <nicolas_boiteux@yahoo.fr>
2014-02-06 08:48:06 +0100

    add deletesection and deletekey method

--- sources/Ini_DB/ini.h
+++ sources/Ini_DB/ini.h
@@ -6,4 +6,6 @@ public:
 
 	static bool Read(char* file, char* section, char* key, char* defaultValue, char* output, size_t outputSize);
 	static bool Write(char* file, char* section, char* key, char* value);
+	static bool Deletesection(char* file, char* section);
+	static bool Deletekey(char* file, char* section, char* key);
 };
